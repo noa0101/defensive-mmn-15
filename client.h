a4a6faee82f1b0fb573d47f4a0c1a14e2628308f
Noa <noabenysrael@gmail.com>
2024-09-24 11:56:20 +0300

    updated code using dynamic memory for request

--- client.h
+++ client.h
@@ -32,54 +32,58 @@ private:
 
 	public:
 		std::string serialize();
-		Request_Header(unsigned char* id, uint8_t ver, uint16_t request_code, uint64_t size);
+		Request_Header(unsigned char* id, uint8_t ver, uint16_t request_code, uint32_t size);
 	};
 
-	class Send_File_Request_Body {
+	class Request_Body {
+	protected:
+		std::string file_name;
+
+	public:
+		Request_Body(std::string name);
+		virtual void send_request_body(std::shared_ptr<tcp::socket>& socket);
+		virtual uint32_t get_len();
+	};
+
+	class Send_Key_Request_Body : public Request_Body {
+		std::string key;
+
+	public:
+		Send_Key_Request_Body(std::string name, std::string key);
+		void send_request_body(std::shared_ptr<tcp::socket>& socket) override;
+		uint32_t get_len() override;
+	};
+
+	class Send_File_Request_Body : public Request_Body {
 		uint32_t content_size;
 		uint32_t orig_size;
 		uint16_t packet_number;
 		uint16_t total_packets;
-		std::string file_name;
-		std::string *message_content;
+		char *message_content;
+
+		std::string serialize_short_fields();
 
 	public:
-		std::string serialize();
-		Send_File_Request_Body(uint32_t content_s, uint32_t orig_s, uint16_t pack_num, uint16_t tot_packs, std::string file_name, std::string *content):
+		Send_File_Request_Body(uint32_t content_s, uint32_t orig_s, uint16_t pack_num, uint16_t tot_packs, std::string file_name, char *content):
 			content_size(content_s), orig_size(orig_s), packet_number(pack_num),
-			total_packets(tot_packs), file_name(file_name), message_content(content) {}
+			total_packets(tot_packs), Request_Body(file_name), message_content(content) {}
+		void send_request_body(std::shared_ptr<tcp::socket>& socket) override;
+		uint32_t get_len() override;
 	};
 
 
 	Request_Header head;
-	std::string payload;
+	Request_Body body;
 
-	Request(unsigned char id[], uint8_t ver, uint16_t code, std::string payload);
+	Request(Request_Header head, Request_Body body);
 	static void check_name_len(std::string file_name);
+	void send_request(std::shared_ptr<tcp::socket>& socket);
 
 public:
-	std::string serialize();
-	static Request generate_general_request(unsigned char id[], uint8_t ver, uint16_t code, std::string name);
-	static Request generate_send_key_request(unsigned char id[], uint8_t ver, uint16_t code, std::string name, std::string key);
-	static Request generate_send_file_request(unsigned char id[], uint8_t ver, uint16_t code, uint32_t content_s, uint32_t orig_s, uint16_t pack_num, uint16_t tot_packs, std::string file_name, std::string *content);
+	static void general_request(std::shared_ptr<tcp::socket>& socket, unsigned char id[], uint8_t ver, uint16_t code, std::string name);
+	static void send_key_request(std::shared_ptr<tcp::socket>& socket, unsigned char id[], uint8_t ver, uint16_t code, std::string name, std::string key);
+	static void send_file_request(std::shared_ptr<tcp::socket>& socket, unsigned char id[], uint8_t ver, uint16_t code, uint32_t content_s, uint32_t orig_s, uint16_t pack_num, uint16_t tot_packs, std::string file_name, char *content);
 };
-
-
-
-
-class Response {
-	class Response_Header {
-		uint8_t version;
-		uint16_t code;
-		uint32_t payload_size;
-
-		Response_Header(std::shared_ptr<tcp::socket>& socket);
-		void read_header(std::shared_ptr<tcp::socket>& socket);
-	};
-};
-
-
-
 
 
 
--- request_handler.cpp
+++ request_handler.cpp
@@ -1,10 +1,5 @@
 #include "client.h"
 
-std::string Request::serialize() {
-    return head.serialize() + payload;
-}
-
-
 template <typename T>
 void Request::add_int_serialization(std::string serialization, T num, uint64_t& offset) {
     T num_le = boost::endian::native_to_little(num);
@@ -26,15 +21,15 @@ std::string Request::Request_Header::serialize() {
     return serialized_head;
 }
 
-Request::Request_Header::Request_Header(unsigned char* id, uint8_t ver, uint16_t request_code, uint64_t size) {
+Request::Request_Header::Request_Header(unsigned char* id, uint8_t ver, uint16_t request_code, uint32_t size) {
     std::copy(id, id + CLIENT_ID_SIZE, client_id);
     version = ver;
     code = request_code;
     payload_size = size;
 }
 
-std::string Request::Send_File_Request_Body::serialize() {
-    std::string serialized_data(sizeof(content_size) + sizeof(orig_size) + sizeof(packet_number) + sizeof(total_packets) + NAME_LEN + content_size, '\0');
+std::string Request::Send_File_Request_Body::serialize_short_fields() {
+    std::string serialized_data(sizeof(content_size) + sizeof(orig_size) + sizeof(packet_number) + sizeof(total_packets) + NAME_LEN, '\0');
     uint64_t offset = 0;
 
     add_int_serialization<uint32_t>(serialized_data, content_size, offset);
@@ -43,14 +38,11 @@ std::string Request::Send_File_Request_Body::serialize() {
     add_int_serialization<uint16_t>(serialized_data, total_packets, offset);
 
     std::memcpy(serialized_data.data() + offset, &file_name, file_name.size());
-    offset += sizeof(NAME_LEN);
-
-    std::memcpy(serialized_data.data() + offset, &message_content, content_size);
 }
 
 
-Request::Request(unsigned char id[], uint8_t ver, uint16_t code, std::string payload) :
-    head(id, ver, code, payload.size()), payload(payload) {}
+Request::Request(Request_Header head, Request_Body body) :
+    head(head), body(body) {}
 
 
 void Request::check_name_len(std::string file_name) {
@@ -58,26 +50,62 @@ void Request::check_name_len(std::string file_name) {
         throw std::runtime_error("Given name is too long, request cannot be generated!");
 }
 
-Request Request::generate_general_request(unsigned char id[], uint8_t ver, uint16_t code, std::string name) {
-    Request::check_name_len(name);
-    return Request(id, ver, code, name);
+void Request::send_request(std::shared_ptr<tcp::socket>& socket) {
+    boost::asio::write(*socket, boost::asio::buffer(head.serialize()));
+    body.send_request_body(socket);
 }
 
-Request Request::generate_send_key_request(unsigned char id[], uint8_t ver, uint16_t code, std::string name, std::string key) {
+void Request::general_request(std::shared_ptr<tcp::socket>& socket, unsigned char id[], uint8_t ver, uint16_t code, std::string name) {
+    Request::check_name_len(name);
+    Request_Body bod(name);
+    Request req(Request_Header(id, ver, code, bod.get_len()), bod);
+    req.send_request(socket);
+}
+
+void Request::send_key_request(std::shared_ptr<tcp::socket>& socket, unsigned char id[], uint8_t ver, uint16_t code, std::string name, std::string key) {
     Request::check_name_len(name);
     if (key.size() != PUBLIC_KEY_SIZE)
         throw std::runtime_error("Public key should be of length " + std::to_string(PUBLIC_KEY_SIZE));
     
-    return Request(id, ver, code, name + key);
+    Send_Key_Request_Body bod(name, key);
+    Request req(Request_Header(id, ver, code, bod.get_len()), bod);
+    req.send_request(socket);
 }
 
-Request::Send_File_Request_Body::Send_File_Request_Body(uint32_t content_s, uint32_t orig_s, uint16_t pack_num, uint16_t tot_packs, std::string file_name, std::string* content) :
+Request::Send_File_Request_Body::Send_File_Request_Body(uint32_t content_s, uint32_t orig_s, uint16_t pack_num, uint16_t tot_packs, std::string file_name, char* content) :
     content_size(content_s), orig_size(orig_s), packet_number(pack_num),
-    total_packets(tot_packs), file_name(file_name), message_content(content) {}
+    total_packets(tot_packs), Request_Body(file_name), message_content(content) {}
 
-Request Request::generate_send_file_request(unsigned char id[], uint8_t ver, uint16_t code, uint32_t content_s, uint32_t orig_s, uint16_t pack_num, uint16_t tot_packs, std::string file_name, std::string *content) {
+void Request::send_file_request(std::shared_ptr<tcp::socket>& socket, unsigned char id[], uint8_t ver, uint16_t code, uint32_t content_s, uint32_t orig_s, uint16_t pack_num, uint16_t tot_packs, std::string file_name, char *content) {
     Request::check_name_len(file_name);
-    Request::Send_File_Request_Body body(content_s, orig_s, pack_num, tot_packs, file_name, content);
-    std::string payload = body.serialize();
-    return Request(id, ver, code, payload);
+    Request::Send_File_Request_Body bod(content_s, orig_s, pack_num, tot_packs, file_name, content);
+    Request req(Request_Header(id, ver, code, bod.get_len()), bod);
+    req.send_request(socket);
+}
+
+Request::Request_Body::Request_Body(std::string name) : file_name(name) {}
+
+uint32_t Request::Request_Body::get_len() {
+    return NAME_LEN;
+}
+
+uint32_t Request::Send_Key_Request_Body::get_len() {
+    return NAME_LEN + PUBLIC_KEY_SIZE;
+}
+
+uint32_t Request::Send_File_Request_Body::get_len() {
+    return sizeof(content_size) + sizeof(orig_size) + sizeof(packet_number) + sizeof(total_packets) + NAME_LEN + content_size;
+}
+
+void Request::Request_Body::send_request_body(std::shared_ptr<tcp::socket>& socket) {
+    boost::asio::write(*socket, boost::asio::buffer(file_name));
+}
+
+void Request::Send_Key_Request_Body::send_request_body(std::shared_ptr<tcp::socket>& socket) {
+    boost::asio::write(*socket, boost::asio::buffer(file_name + key));
+}
+
+void Request::Send_File_Request_Body::send_request_body(std::shared_ptr<tcp::socket>& socket) {
+    boost::asio::write(*socket, boost::asio::buffer(serialize_short_fields()));
+    boost::asio::write(*socket, boost::asio::buffer(message_content, content_size));
 }
